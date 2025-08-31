88edcaa593e3f2e9ed3a6795e69dce2fb9ba93df
ShreyaSarkar2803 <163161206+ShreyaSarkar2803@users.noreply.github.com>
2025-09-01 01:08:03 +0530

    initial frontend setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// export default function App() {
+//   return (
+//     <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+//       <h1 className="text-4xl font-bold text-blue-400">🚀 Tailwind is working!</h1>
+//     </div>
+//   );
+// }
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center text-2xl">
+      Frontend up & running 🚀
+    </div>
+  );
+}
